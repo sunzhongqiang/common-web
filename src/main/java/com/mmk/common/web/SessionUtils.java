daa7cfd6530b8b726fe12df32666318247d08671
孙中强 <sunzhongqiang@126.com>
2019-06-24 17:14:35 +0800

    增强session方便取值

--- src/main/java/com/mmk/common/web/SessionUtils.java
+++ src/main/java/com/mmk/common/web/SessionUtils.java
@@ -1,5 +1,6 @@
 package com.mmk.common.web;
 
+import java.math.BigDecimal;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.web.context.request.RequestAttributes;
@@ -10,6 +11,65 @@ public class SessionUtils {
 
   public static HttpSession getSession() {
    return WebUtils.getSession();
+  }
+
+
+  public static void set(String name,Object value){
+    getSession().setAttribute(name,value);
+  }
+
+
+  public static Object get(String name){
+    return getSession().getAttribute(name);
+  }
+
+
+  public static String getString(String name){
+    Object value = get(name);
+    if(value  == null){
+      return null;
+    }
+    return value.toString();
+  }
+
+  public static Long getLong(String name){
+    Object value = get(name);
+    if(value  == null){
+      return null;
+    }
+    return Long.valueOf(value.toString());
+  }
+
+  public static Integer getInteger(String name){
+    Object value = get(name);
+    if(value  == null){
+      return null;
+    }
+    return Integer.valueOf(value.toString());
+  }
+
+  public static Double getDouble(String name){
+    Object value = get(name);
+    if(value  == null){
+      return null;
+    }
+    return Double.valueOf(value.toString());
+  }
+
+  public static BigDecimal getBigDecimal(String name){
+    Object value = get(name);
+    if(value  == null){
+      return null;
+    }
+    return BigDecimal.valueOf(Double.valueOf(value.toString()));
+  }
+
+  public static  <T> T getObject(String name){
+    Object value = get(name);
+    if(value  == null){
+      return null;
+    }
+    return  (T) value;
   }
 
 }
